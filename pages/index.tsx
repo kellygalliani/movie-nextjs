b2f5cdd47c6c4e98d0faa9fbbba8c39a1a71e1ff
kelly galliani <kelly_92b@hotmail.com>
2023-06-17 16:57:10 -0300

    feat: movies api and cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import BillBoard from "@/components/BillBoard";
+import { useEffect } from "react";
+
+import BillBoard from "@/components/Billboard";
 import Navbar from "@/components/Navbar";
-import useCurrentUser from "@/hooks/useCurrentUser";
+import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
-import { useEffect } from "react";
+import MovieList from "@/components/MovieList";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -22,12 +24,15 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
 
   return (
     <>
       <Navbar />
       <BillBoard />
+      <div className="pb-40">
+        <MovieList data={movies} title="Trending Now" />
+      </div>
     </>
   );
 }
